c5cf61cd711095de244904626b0c823e247682d3
Vinicius Clemente da Silva Quintino <vinicius.clemente@compwire.com.br>
2024-05-10 16:50:50 -0300

    test

--- config.sh
+++ config.sh
@@ -118,8 +118,8 @@ cd archives/
 # Install Aquatone
 echo "[+] Instalando Aquatone"
 mkdir aquatone
-cd aquatone
 mv aquatone_linux_amd64_1.7.0.zip aquatone
+cd aquatone
 unzip aquatone_linux_amd64_1.7.0.zip
 rm aquatone_linux_amd64_1.7.0.zip
 cd ..
