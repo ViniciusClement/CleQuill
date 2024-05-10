9318e0f535145c8b524f001b8240940a318dd738
Vinicius Clemente da Silva Quintino <vinicius.clemente@compwire.com.br>
2024-05-10 16:45:17 -0300

    Upload

--- config.sh
+++ config.sh
@@ -58,61 +58,62 @@
 #apt-get install nikto -y
 #apt-get install dnsrecon -y
 #pip3 install metafinder --upgrade
+#sudo apt-get install p7zip-full
 ####
 
 cd archives/
 
-# Install Sudomy
-echo "[+] Instalando Sudomy"
-unzip Sudomy-master.zip
-rm -rf Sudomy-master.zip
-
-# Install ctfr
-echo "[+] Instalando Ctfr"
-unzip ctfr-master.zip
-rm -rf ctfr-master.zip
-
-# Install Sublist3r
-echo "[+] Instalando Sublist3r"
-unzip Sublist3r-master.zip
-rm -rf Sublist3r-master.zip
-
-# Install Httpx
-echo "[+] Instalando Httpx"
-mkdir httpx
-cd httpx
-unzip httpx_1.6.0_linux_amd64.zip
-rm -rf httpx_1.6.0_linux_amd64.zip
-cd ..
-
-# Install gau
-echo "[+] Instalando Gau"
-mkdir gau
-cd gau
-tar xvf gau_2.2.1_linux_amd64.tar.gz
-rm gau_2.2.1_linux_amd64.tar.gz
-cd ..
-
-# Install Gf
-echo "[+] Instalando Gf"
-unzip gf-master.zip
-rm -rf Gf-Patterns-master.zip
-cd gf-master
-go build main.go
-mv main gf
-cp gf /usr/bin
-mkdir /root/.gf/
-cd examples	
-cp * /root/.gf/
-cd ..
-cd ..
-
-# Config. Gf
-unzip Gf-Patterns-master.zip
-rm -rf Gf-Patterns-master.zip
-cd Gf-Patterns
-cp * /root/.gf/
-cd ..
+## Install Sudomy
+#echo "[+] Instalando Sudomy"
+#unzip Sudomy-master.zip
+#rm -rf Sudomy-master.zip
+#
+## Install ctfr
+#echo "[+] Instalando Ctfr"
+#unzip ctfr-master.zip
+#rm -rf ctfr-master.zip
+#
+## Install Sublist3r
+#echo "[+] Instalando Sublist3r"
+#unzip Sublist3r-master.zip
+#rm -rf Sublist3r-master.zip
+#
+## Install Httpx
+#echo "[+] Instalando Httpx"
+#mkdir httpx
+#cd httpx
+#unzip httpx_1.6.0_linux_amd64.zip
+#rm -rf httpx_1.6.0_linux_amd64.zip
+#cd ..
+#
+## Install gau
+#echo "[+] Instalando Gau"
+#mkdir gau
+#cd gau
+#tar xvf gau_2.2.1_linux_amd64.tar.gz
+#rm gau_2.2.1_linux_amd64.tar.gz
+#cd ..
+#
+## Install Gf
+#echo "[+] Instalando Gf"
+#unzip gf-master.zip
+#rm -rf Gf-Patterns-master.zip
+#cd gf-master
+#go build main.go
+#mv main gf
+#cp gf /usr/bin
+#mkdir /root/.gf/
+#cd examples	
+#cp * /root/.gf/
+#cd ..
+#cd ..
+#
+## Config. Gf
+#unzip Gf-Patterns-master.zip
+#rm -rf Gf-Patterns-master.zip
+#cd Gf-Patterns
+#cp * /root/.gf/
+#cd ..
 
 # Install Aquatone
 echo "[+] Instalando Aquatone"
@@ -122,79 +123,80 @@ unzip aquatone_linux_amd64_1.7.0.zip
 rm aquatone_linux_amd64_1.7.0.zip
 cd ..
 
-# Install Dirsearch
-echo "[+] Instalando Dirsearch"
-unzip dirsearch-master.zip
-rm -rf dirsearch-master.zip
-
-# Copy Gau to bin 
-cd gau/
-cp gau /usr/bin
-cd ..
-
-# Install SecretFinder
-echo "[+] Instalando SecretFinder"
-unzip SecretFinder-master.zip
-rm -rf SecretFinder-master.zip
-
-# Install yagooglesearch
-echo "[+] Instalando Yagooglesearch"
-unzip yagooglesearch-master.zip
-rm -rf yagooglesearch-master.zip
-cd yagooglesearch
-virtualenv -p python3 .venv  # If using a virtual environment.
-source .venv/bin/activate  # If using a virtual environment.
-pip install .  # Reads from pyproject.toml
-source desactivate
-cd ..
-
-# Install Pagodo
-echo "[+] Instalando Pagodo"
-tar xvzf pagodo-2.6.2.tar.gz
-mv pagodo-2.6.2 pagodo  
-rm -rf pagodo-2.6.2.tar.gz
-
-# Install EmailHarvester
-echo "[+] Instalando EmailHarvester"
-mkdir Emailharvester
-apt-get install emailharvester -y
-
-# Install Go-Dork
-echo "[+] Instalando Go-Dork"
-mkdir go-dork
-mv go-dork_1.0.3_linux_amd64 go-dork
-cd go-dork
-chmod u+x go-dork_1.0.3_linux_amd64
-mkdir output
-cd ..
-
-# Install CloudFlair
-echo "[+] Instalando CloudFlair"
-unzip CloudFlair-master.zip
-rm -rf CloudFlair-master.zip
-
-# Install H8mail
-echo "[+] Instalando H8mail"
-unzip h8mail-2.5.6.zip
-rm -rf h8mail-2.5.6.zip
-cd h8mail-2.5.6
-make install
-cd ..
-
-# Install LinkFinder
-echo "[+] Instalando LinkFinder"
-unzip LinkFinder-master.zip
-rm -rf LinkFinder-master.zip
-
-# Install MetaFinder
-unzip LinkFinder-master.zip
-rm -rf LinkFinder-master.zip
-
-# Install Imperva-detect
-unzip imperva-detect-master.zip
-rm -rf imperva-detect-master.zip
-cd imperva-detect-master
-chmod +x check_ciphers.sh
-chmod +x imperva-detect.sh
-
-
+## Install Dirsearch
+#echo "[+] Instalando Dirsearch"
+#unzip dirsearch-master.zip
+#rm -rf dirsearch-master.zip
+#
+## Copy Gau to bin 
+#cd gau/
+#cp gau /usr/bin
+#cd ..
+#
+## Install SecretFinder
+#echo "[+] Instalando SecretFinder"
+#unzip SecretFinder-master.zip
+#rm -rf SecretFinder-master.zip
+#
+## Install yagooglesearch
+#echo "[+] Instalando Yagooglesearch"
+#unzip yagooglesearch-master.zip
+#rm -rf yagooglesearch-master.zip
+#cd yagooglesearch
+#virtualenv -p python3 .venv  # If using a virtual environment.
+#source .venv/bin/activate  # If using a virtual environment.
+#pip install .  # Reads from pyproject.toml
+#source desactivate
+#cd ..
+#
+## Install Pagodo
+#echo "[+] Instalando Pagodo"
+#tar xvzf pagodo-2.6.2.tar.gz
+#mv pagodo-2.6.2 pagodo  
+#rm -rf pagodo-2.6.2.tar.gz
+#
+## Install EmailHarvester
+#echo "[+] Instalando EmailHarvester"
+#mkdir Emailharvester
+#apt-get install emailharvester -y
+#
+## Install Go-Dork
+#echo "[+] Instalando Go-Dork"
+#mkdir go-dork
+#mv go-dork_1.0.3_linux_amd64 go-dork
+#cd go-dork
+#chmod u+x go-dork_1.0.3_linux_amd64
+#mkdir output
+#cd ..
+#
+## Install CloudFlair
+#echo "[+] Instalando CloudFlair"
+#unzip CloudFlair-master.zip
+#rm -rf CloudFlair-master.zip
+#
+## Install H8mail
+#echo "[+] Instalando H8mail"
+#unzip h8mail-2.5.6.zip
+#rm -rf h8mail-2.5.6.zip
+#cd h8mail-2.5.6
+#make install
+#cd ..
+#
+## Install LinkFinder
+#echo "[+] Instalando LinkFinder"
+#unzip LinkFinder-master.zip
+#rm -rf LinkFinder-master.zip
+#
+## Install MetaFinder
+#unzip LinkFinder-master.zip
+#rm -rf LinkFinder-master.zip
+#
+## Install Imperva-detect
+#unzip imperva-detect-master.zip
+#rm -rf imperva-detect-master.zip
+#cd imperva-detect-master
+#chmod +x check_ciphers.sh
+#chmod +x imperva-detect.sh
+#
+#
+#
